6add2a0ff1d70b685db174f46a6dcbbfa01bee9d
pathway-release-manul <157591932+pathway-release-manul@users.noreply.github.com>
2024-01-24 13:45:04 +0100

    correct rag backend host and port

--- demo/rag.py
+++ demo/rag.py
@@ -3,9 +3,8 @@ from llama_index.chat_engine.condense_question import CondenseQuestionChatEngine
 from llama_index.query_engine import RetrieverQueryEngine
 from llama_index.retrievers import PathwayRetriever
 
-PATHWAY_HOST = "https://api-pathway-indexer.staging.deploys.pathway.com/query"
-PATHWAY_PORT = "80"
-
+PATHWAY_HOST = "api-pathway-indexer.staging.deploys.pathway.com"
+PATHWAY_PORT = 80
 
 load_dotenv()
 
