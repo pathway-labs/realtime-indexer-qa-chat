48e056dc5f1b4dd97aea1e11e9a90435ce3be13d
Berke <berkecanrizai1@gmail.com>
2024-01-25 14:28:36 +0100

    fix: ui, init message, fixes, refactor

--- demo/app.py
+++ demo/app.py
@@ -3,41 +3,119 @@ from dotenv import load_dotenv
 
 with st.sidebar:
     st.markdown(
-        "[View the source code on GitHub](https://github.com/pathwaycom/pathway)"
+        "[View code on GitHub.](https://github.com/pathwaycom/pathway)"
     )
 
     st.markdown(
-        """
-        This demo showcases Pathway pipeline that is connected to a Google Drive Folder and Sharepoint. Pathway LlamaIndex integration powers the rag application by providing synced files to LLM. To see more about Pathway's LlamaIndex integration, see more about it in [this blog post](https://pathway.com/developers/showcases/llamaindex-pathway/) and LlamaIndex docs [Pathway Reader](https://docs.llamaindex.ai/en/stable/examples/data_connectors/PathwayReaderDemo.html#pathway-reader), [Pathway Retriever docs](https://docs.llamaindex.ai/en/stable/examples/retrievers/pathway_retriever.html#pathway-retriever).
-        """
+        """Pathway pipelines ingest documents from [Google Drive](https://drive.google.com/drive/u/0/folders/1cULDv2OaViJBmOfG5WB0oWcgayNrGtVs) and [Sharepoint](https://navalgo.sharepoint.com/:f:/s/ConnectorSandbox/EgBe-VQr9h1IuR7VBeXsRfIBuOYhv-8z02_6zf4uTH8WbQ?e=YmlA05). It automatically manages and syncs indexes enabling RAG applications."""
     )
 
     st.markdown(
-        "Check out Pathway hosted index on [Pathway Indexer](https://pathway-indexer.staging.deploys.pathway.com/)"
+        """Ready to build your own? 
+
+Our [docs](https://pathway.com/developers/showcases/llamaindex-pathway/) walk through creating custom pipelines with LlamaIndex.
+
+Want a hosted version?
+
+Check out our [hosted document pipelines](https://pathway-indexer.staging.deploys.pathway.com/)."""
     )
 
-    st.markdown(
-        """Add your documents 📄📩 to [Drive Folder](https://drive.google.com/drive/u/0/folders/1cULDv2OaViJBmOfG5WB0oWcgayNrGtVs) 
-                or [Sharepoint](https://navalgo.sharepoint.com/:f:/s/ConnectorSandbox/EgBe-VQr9h1IuR7VBeXsRfIBuOYhv-8z02_6zf4uTH8WbQ?e=YmlA05)"""
-    )
 
 # Load environment variables
 load_dotenv()
 
 
 # Streamlit UI elements
-st.title("Pathway 😺 + LlamaIndex 🦙")
+st.title("Chat with documents realtime ⚡ in Google Drive & Sharepoint")
+
+
+# st.markdown("#### Built with:")
+# st.image('assets/combinedhosted.png', width=300)
+
+# col1, mid, col2 = st.columns([1,1,1])
+# with col1:
+#     st.text('built with')
+# with col2:
+#     st.image('assets/combinedhosted.png', width=300)
+
+
+import base64
+
+LOGO_IMAGE = "assets/combinedhosted.png"
+
+# st.markdown(
+#     """
+#     <style>
+#     .container {
+#         display: flex;
+#     }
+#     .logo-text {
+#         font-weight:700 !important;
+#         font-size:50px !important;
+#         color: #f9a01b !important;
+#         padding-top: 75px !important;
+#     }
+#     .logo-img {
+#         float:right;
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+# st.markdown(
+#     f"""
+#     <div class="container">
+#         <img class="logo-img" src="./app/static/sharepoint.png" width="50" alt="Google Drive Logo"">
+#         <p class="logo-text"> Some text </p>
+#     </div>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+htt = """
+<p>
+    <span> Built With: </span>
+    <img src="./app/static/combinedhosted.png" width="300" alt="Google Drive Logo">
+</p>
+"""
+st.markdown(htt, unsafe_allow_html=True)
+
+#st.markdown("[![Upload](demo/static/Google_Drive_logo.png)](https://drive.google.com/drive/u/0/folders/1cULDv2OaViJBmOfG5WB0oWcgayNrGtVs)")
+# st.markdown("[![Upload](./app/static/Google_Drive_logo.png)](https://drive.google.com/drive/u/0/folders/1cULDv2OaViJBmOfG5WB0oWcgayNrGtVs)")
+
+image_width = 300
+image_height = 200
+
+htm = """
+<div style="display: flex; ">
+  <a href="https://drive.google.com/drive/u/0/folders/1cULDv2OaViJBmOfG5WB0oWcgayNrGtVs">
+    <figure style="text-align: center; margin-right: 10px;">
+      <img src="./app/static/Google_Drive_logo.png" width="50" alt="Google Drive Logo">
+      <figcaption>Upload to Google Drive</figcaption>
+    </figure>
+  </a>
+
+  <a href="https://navalgo.sharepoint.com/:f:/s/ConnectorSandbox/EgBe-VQr9h1IuR7VBeXsRfIBuOYhv-8z02_6zf4uTH8WbQ?e=YmlA05">
+    <figure style="text-align: center;">
+      <img src="./app/static/sharepoint.png" width="50" alt="Google Drive Logo">
+      <figcaption>Upload to Sharepoint</figcaption>
+    </figure>
+  </a>
+</div>
+"""
+
+st.markdown(htm, unsafe_allow_html=True)
+
 
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [
-        {
-            "role": "assistant",
-            "content": "Hi, ask me a question. My knowledge is always up to date!",
-        }
-    ]
     from rag import chat_engine
 
-    st.session_state.chat_engine = chat_engine
+    st.session_state.messages = [{'role': msg.role, 'content': msg.content} for msg in chat_engine.chat_history]
+    # st.session_state.chat_engine = chat_engine
+    # for msg in chat_engine.chat_history:
+    #     st.session_state.messages.append({'role': msg.role, 'content': msg.content})
+        
 
 
 if prompt := st.chat_input("Your question"):
--- demo/rag.py
+++ demo/rag.py
@@ -3,6 +3,7 @@ from llama_index.chat_engine.condense_plus_context import \
     CondensePlusContextChatEngine
 from llama_index.query_engine import RetrieverQueryEngine
 from llama_index.retrievers import PathwayRetriever
+from llama_index.llms.types import ChatMessage, MessageRole
 
 PATHWAY_HOST = "api-pathway-indexer.staging.deploys.pathway.com"
 PATHWAY_PORT = 80
@@ -15,9 +16,11 @@ query_engine = RetrieverQueryEngine.from_args(
     retriever,
 )
 
+pathway_explaination = "Pathway is a high-throughput, low-latency data processing framework that handles live data & streaming for you."
 
 chat_engine = CondensePlusContextChatEngine.from_defaults(
     retriever=retriever,
     system_prompt="IF QUESTION IS NOT RELATED TO CONTEXT DOCUMENTS, SAY IT'S NOT POSSIBLE TO ANSWER.",
     verbose=True,
+    chat_history=[ChatMessage(role=MessageRole.USER, content='What is Pathway?'), ChatMessage(role=MessageRole.ASSISTANT, content=pathway_explaination)]
 )
