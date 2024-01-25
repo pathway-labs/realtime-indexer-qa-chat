094dc41fb9fbdfb728fc7efa6b00fba5018503b9
Berke <berkecanrizai1@gmail.com>
2024-01-25 14:29:36 +0100

    fix: rm comments

--- demo/app.py
+++ demo/app.py
@@ -29,50 +29,6 @@ load_dotenv()
 st.title("Chat with documents realtime ⚡ in Google Drive & Sharepoint")
 
 
-# st.markdown("#### Built with:")
-# st.image('assets/combinedhosted.png', width=300)
-
-# col1, mid, col2 = st.columns([1,1,1])
-# with col1:
-#     st.text('built with')
-# with col2:
-#     st.image('assets/combinedhosted.png', width=300)
-
-
-import base64
-
-LOGO_IMAGE = "assets/combinedhosted.png"
-
-# st.markdown(
-#     """
-#     <style>
-#     .container {
-#         display: flex;
-#     }
-#     .logo-text {
-#         font-weight:700 !important;
-#         font-size:50px !important;
-#         color: #f9a01b !important;
-#         padding-top: 75px !important;
-#     }
-#     .logo-img {
-#         float:right;
-#     }
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-# )
-
-# st.markdown(
-#     f"""
-#     <div class="container">
-#         <img class="logo-img" src="./app/static/sharepoint.png" width="50" alt="Google Drive Logo"">
-#         <p class="logo-text"> Some text </p>
-#     </div>
-#     """,
-#     unsafe_allow_html=True
-# )
-
 htt = """
 <p>
     <span> Built With: </span>
@@ -81,8 +37,6 @@ htt = """
 """
 st.markdown(htt, unsafe_allow_html=True)
 
-#st.markdown("[![Upload](demo/static/Google_Drive_logo.png)](https://drive.google.com/drive/u/0/folders/1cULDv2OaViJBmOfG5WB0oWcgayNrGtVs)")
-# st.markdown("[![Upload](./app/static/Google_Drive_logo.png)](https://drive.google.com/drive/u/0/folders/1cULDv2OaViJBmOfG5WB0oWcgayNrGtVs)")
 
 image_width = 300
 image_height = 200
@@ -112,10 +66,6 @@ if "messages" not in st.session_state.keys():
     from rag import chat_engine
 
     st.session_state.messages = [{'role': msg.role, 'content': msg.content} for msg in chat_engine.chat_history]
-    # st.session_state.chat_engine = chat_engine
-    # for msg in chat_engine.chat_history:
-    #     st.session_state.messages.append({'role': msg.role, 'content': msg.content})
-        
 
 
 if prompt := st.chat_input("Your question"):
